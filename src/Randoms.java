35a9cd77b8a878e856e6384046f531c5376dfcbb
Ilia Shudegov <faun-08@mail.ru>
2023-02-18 00:40:20 +0300

    Second commit

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,20 +1,17 @@
-import java.util.ArrayList;
+
 import java.util.Iterator;
-import java.util.List;
+
 import java.util.Random;
 
 public class Randoms implements Iterable<Integer> {
 
     protected Random random = new Random();
-    public List<Integer> list = new ArrayList<>();
 
     public Randoms(int min, int max) {
         int diff =max -min;
-        while (diff !=0) {
+        while (true) {
             int result = random.nextInt(diff + 1);
             result+=min;
-            list.add(result);
-            diff--;
         }
     }
 
@@ -25,21 +22,13 @@ public class Randoms implements Iterable<Integer> {
 
             @Override
             public boolean hasNext() {
-                if (index < list.size())
-                    return true;
                 return false;
             }
 
 
             @Override
             public Integer next() {
-                if (index < list.size()) {
-                    int number = list.get(index);
-                    index++;
-                    return number;
-                } else {
-                    return 0;
-                }
+                return 0;
             }
         };
     }
